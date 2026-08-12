bfe9b7a9c4c0f6a3b71651955227e4a84f4f918c
prem sai <your_email@example.com>
2026-08-12 16:42:03 +0530

    Update DiseaseX app title and meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,8 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
         <Head>
-          <title>ProctoAi</title>
-          <meta name="description" content="ProctoAi - Smart Exam Inviligator" />
+          <title>DiseaseX Healthcare Platform</title>
+          <meta name="description" content="DiseaseX - Multi-Disease Prediction Platform" />
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         </Head>
