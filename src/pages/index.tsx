563a12f7dfb6ed110ffeb0572fdb9d716ee6cc18
prem sai <your_email@example.com>
2026-08-12 16:32:33 +0530

    Render HomePage directly on index route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,3 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import HomePage from './home';
 
-export default function Home() {
-  const router = useRouter();
-  
-  useEffect(() => {
-    router.push('/home');
-  }, [router]);
-  
-  return null;
-}
+export default HomePage;
